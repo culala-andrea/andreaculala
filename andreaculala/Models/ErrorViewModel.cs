4b6e8a9766fd431bc826bbb2f2a004b900a1cedf
CpELab-PC <CpELab-PC@CPELAB-PC8>
2025-01-10 11:30:41 +0800

    Add project files.

---
+++ andreaculala/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace andreaculala.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
